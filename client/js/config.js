56d2a5f54832c4d341175fbf0bbfcf2912398500
Prachi Kolte <kolteprachi10@gmail.com>
2026-07-06 10:37:43 +0530

    Update config.js

--- client/js/config.js
+++ client/js/config.js
@@ -1,5 +1,1 @@
-const API_BASE_URL =
-    window.location.hostname === "localhost" ||
-    window.location.hostname === "127.0.0.1"
-        ? "https://ai-posts-scheduler-production.up.railway.app"
-        : "https://YOUR-RENDER-URL.onrender.com";
+const API_BASE_URL = "https://ai-posts-scheduler-production.up.railway.app";
